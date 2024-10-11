48bf0ee0788676cc24aeda9b97f35336e5e7f361
demirose41 <demirose41@gmail.com>
2024-10-11 01:21:26 -0700

    completed practice

--- solution/dynamic-array.js
+++ solution/dynamic-array.js
@@ -2,18 +2,25 @@ class DynamicArray {
 
   constructor(defaultSize=4) {
 
-    // Your code here
+    this.data = new Array(defaultSize);
+    this.length = 0;
+    this.capacity = defaultSize;
 
   }
 
   read(index) {
 
-    // Your code here
+    return this.data[index];
   }
 
   unshift(val) {
 
-    // Your code here
+    for(let i = this.data.length; i >= 0; i--){
+      this.data[i+1] = this.data[i];
+    }
+
+    this.data[0] = val;
+    this.length += 1;
   }
 
 }
